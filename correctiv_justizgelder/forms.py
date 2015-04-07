dbedc896301926f3b177bfbc0da45ab3c8b8d1e6
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2015-04-07 18:10:33 +0200

    Add new years to form

--- correctiv_justizgelder/forms.py
+++ correctiv_justizgelder/forms.py
@@ -32,9 +32,14 @@ class OrganisationSearchForm(forms.Form):
 
     year = forms.TypedChoiceField(
         choices=(
+            (2007, '2007'),
+            (2008, '2008'),
+            (2009, '2009'),
+            (2010, '2010'),
             (2011, '2011'),
             (2012, '2012'),
-            (2013, '2013')
+            (2013, '2013'),
+            (2014, '2014')
         ),
         required=False,
         coerce=int,
