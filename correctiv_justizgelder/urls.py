3ccb4248813d37b4e2c98c40dba2e6162a8b390a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2015-04-29 13:21:09 +0200

    Only cache anonymous pages

--- correctiv_justizgelder/urls.py
+++ correctiv_justizgelder/urls.py
@@ -1,14 +1,31 @@
 from django.conf.urls import patterns, url
 from django.utils.translation import ugettext_lazy as _
-from django.views.decorators.cache import cache_page
+from django.utils.decorators import decorator_from_middleware_with_args
+from django.middleware.cache import CacheMiddleware
 
 from .views import OrganisationSearchView, OrganisationDetail
 
 CACHE_TIME = 15 * 60
 
 
+def cache_page_anonymous(*args, **kwargs):
+    """
+    Decorator to cache Django views only for anonymous users.
+    Use just like the decorator cache_page:
+
+    @cache_page_anonymous(60 * 30)  # cache for 30 mins
+    def your_view_here(request):
+        ...
+    """
+    key_prefix = kwargs.pop('key_prefix', None)
+    return decorator_from_middleware_with_args(CacheMiddleware)(
+        cache_timeout=args[0],
+        key_prefix=key_prefix,
+        cache_anonymous_only=True)
+
+
 def c(view):
-    return cache_page(CACHE_TIME)(view)
+    return cache_page_anonymous(CACHE_TIME)(view)
 
 
 urlpatterns = patterns('',
