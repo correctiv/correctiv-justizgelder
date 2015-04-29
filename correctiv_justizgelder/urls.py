0b2d9d94c0aad58b7c9c6d30256f2a7237c4c3d0
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2015-04-29 10:57:33 +0200

    Cache database views

--- correctiv_justizgelder/urls.py
+++ correctiv_justizgelder/urls.py
@@ -1,14 +1,19 @@
 from django.conf.urls import patterns, url
 from django.utils.translation import ugettext_lazy as _
+from django.views.decorators.cache import cache_page
 
 from .views import OrganisationSearchView, OrganisationDetail
 
+CACHE_TIME = 15 * 60
+
+
+def c(view):
+    return cache_page(CACHE_TIME)(view)
+
 
 urlpatterns = patterns('',
-    url(r'^$',
-        OrganisationSearchView.as_view(),
-        name='search'),
+    url(r'^$', c(OrganisationSearchView.as_view()), name='search'),
     url(_(r'^recipient/(?P<slug>[\w-]+)/$'),
-        OrganisationDetail.as_view(),
+        c(OrganisationDetail.as_view()),
         name='organisation_detail'),
 )
