5010d9da352ee2a44b156401a773e14992e3afd3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-08-18 16:19:35 +0200

    Refactor year choice field in forms

--- correctiv_justizgelder/forms.py
+++ correctiv_justizgelder/forms.py
@@ -40,17 +40,11 @@ class OrganisationSearchForm(forms.Form):
         coerce=lambda x: None
     )
 
+    MIN_YEAR, MAX_YEAR = 2007, 2016
     year = forms.TypedChoiceField(
-        choices=(
-            (2007, '2007'),
-            (2008, '2008'),
-            (2009, '2009'),
-            (2010, '2010'),
-            (2011, '2011'),
-            (2012, '2012'),
-            (2013, '2013'),
-            (2014, '2014')
-        ),
+        choices=[
+            (year, str(year)) for year in range(MIN_YEAR, MAX_YEAR + 1)
+        ],
         required=False,
         coerce=int,
         empty_value='',
